a85cbe3130d2c2997f8d2838008664d560c0968a
rb <rb@rbs-MacBook-Pro.local>
2024-05-08 23:55:16 +0530

    working on scroll bar

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Header, { SearchComponent } from './components/Header';
-import { multiCategory } from './api/Dashboard';
+import { clothes, multiCategory, phoneCategory } from './api/Dashboard';
 
 function App() {
   return (
@@ -22,24 +22,47 @@ function App() {
                 <img className='' style={{backgroundColor:'#ffffff'}} src={`${item.url}`} alt="" />
                 <label title=''>{`${item.title} ${index}`}</label>
                 </div>
-             </>})
+             </>
+             })
         }
         </div>
 <>
-<div className='card margin-top' >
-<label title=''>{`Top Deals`}</label>
+<div className='card margin-top phone-categotry' >
+<label title=''>{`${phoneCategory.title}`}</label>
           <div className="card top-deals-view margin-top">
-            {
-              multiCategory.map((item, index)=> {
-                return <>
+                      <div className='categoryRow' style={{ overflowX: 'scroll', whiteSpace: 'nowrap' }}>
+                      {
+                        phoneCategory.products.map((item, index)=> {
+                          return <>
+                              <div className='top-deals-card'>
+                              <img className='' style={{backgroundColor:'#ffffff'}} src={`${item.thumbnail}`} alt="" />
+                              <label title=''>{`${item.title}`}</label>
+                              </div>
+                          </>})
+                      }
+                   </div>
+          </div>
+        </div>
+</>
 
-                    <div className='top-deals-card'>
-                    <img className='' style={{backgroundColor:'#ffffff'}} src={`${item.url}`} alt="" />
-                    <label title=''>{`${item.title} ${index}`}</label>
-                    </div>
-                </>})
-            }
-            </div>
+<>
+<div className='card margin-top phone-categotry' >
+<label title=''>{`${clothes.title}`}</label>
+          <div className="card top-deals-view margin-top">
+                      <div className='categoryRow' style={{ overflowX: 'scroll', whiteSpace: 'nowrap' }}>
+                      {
+                        clothes.product.map((item, index)=> {
+                          return <>
+                              <div className='top-deals-card'>
+                              <img className='' style={{backgroundColor:'#ffffff'}} src={`${item.image}`} alt="" />
+                              <label title=''>{`${item.title}`}</label>
+                              <label className='from-price'>{`From ${item.price}`}</label>
+
+                              </div>
+                          </>})
+                      }
+                   </div>
+          </div>
         </div>
 </>
   </div>
